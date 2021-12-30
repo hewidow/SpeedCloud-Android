d6fc847e8ae38847796aef1d58f4e57b4c1ea515
Top光No.1 <844969710@qq.com>
2021-12-31 00:00:41 +0800

    toolbar样式

--- app/src/main/java/com/example/speedcloud/fragment/FileFragment.kt
+++ app/src/main/java/com/example/speedcloud/fragment/FileFragment.kt
@@ -1,14 +1,19 @@
 package com.example.speedcloud.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.example.speedcloud.R
+import com.google.android.material.appbar.AppBarLayout
+import kotlin.math.abs
 
-class FileFragment : Fragment() {
+class FileFragment : Fragment(), AppBarLayout.OnOffsetChangedListener {
 
+    private lateinit var root: View
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -18,9 +23,18 @@ class FileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_file, container, false)
+        root = inflater.inflate(R.layout.fragment_file, container, false)
+        toolbar = root.findViewById(R.id.toolbar)
+        root.findViewById<AppBarLayout>(R.id.appbar)
+            .addOnOffsetChangedListener(this) // 监听appbar处于折叠还是展开
+        return root
+    }
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+        if (abs(verticalOffset) >= appBarLayout.totalScrollRange) {
+        }
     }
 
     companion object {
