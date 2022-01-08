085ed3a31ef753b97b425dc07a4db969e3495c4d
RDJ.Widow <844969710@qq.com>
2022-01-08 11:20:01 +0800

    优化用户体验

--- app/src/main/java/com/example/speedcloud/receiver/CompleteReceiver.kt
+++ app/src/main/java/com/example/speedcloud/receiver/CompleteReceiver.kt
@@ -4,7 +4,6 @@ import android.app.DownloadManager.EXTRA_DOWNLOAD_ID
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.example.speedcloud.MainApplication
 import com.example.speedcloud.util.DownloadManagerUtil
 
@@ -14,7 +13,6 @@ import com.example.speedcloud.util.DownloadManagerUtil
 class CompleteReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         val task = intent.getLongExtra(EXTRA_DOWNLOAD_ID, -1)
-        Log.d("hgf", "${DownloadManagerUtil.getDownloadProgress(task).third}")
         if (DownloadManagerUtil.getDownloadProgress(task).third == 0) { // 若是取消的，就删除记录
             MainApplication.getInstance().swapDataBase.swapNodeDao().deleteByTask(task)
         } else { // 完成就更新记录
